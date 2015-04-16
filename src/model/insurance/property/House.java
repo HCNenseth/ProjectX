c6079d27057fcbd4a6edacf62ff7a51367932c9c
Alexander Skjolden <alex@plastboks.net>
2015-04-16 12:56:39 +0200

    added house {type, material, standard} - code
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/model/insurance/property/House.java
+++ src/model/insurance/property/House.java
@@ -1,5 +1,6 @@
 package model.insurance.property;
 
+import localization.Loc;
 import model.Person;
 
 import java.util.Calendar;
@@ -13,10 +14,42 @@ public class House extends Property
     private short postalCode;
     private String city;
     private int year;
-    private enum type {A, B, C}
-    private enum material {A, B, C}
-    private enum standard {A, B, C}
     private int squareMeter;
+
+    public enum Type {
+        A(Loc.get("house_type_a")),
+        B(Loc.get("house_type_b")),
+        C(Loc.get("house_type_c"));
+
+        String value;
+
+        Type(String value) { this.value = value; }
+
+        public String getValue() { return value; }
+
+    }
+    public enum Material {
+        A(Loc.get("house_material_a")),
+        B(Loc.get("house_material_b")),
+        C(Loc.get("house_material_c"));
+
+        String value;
+
+        Material(String value) { this.value = value; }
+
+        public String getValue() { return value; }
+    }
+    public enum Standard {
+        A(Loc.get("house_standard_a")),
+        B(Loc.get("house_standard_b")),
+        C(Loc.get("house_standard_c"));
+
+        String value;
+
+        Standard(String value) { this.value = value; }
+
+        public String getValue() { return value; }
+    }
 
     public House(Person customer, double premium, double amount, Calendar date, String desc)
     {
