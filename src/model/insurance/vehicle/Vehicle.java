245ca2493d2522ce75f80741e3babf6b2ea5f55e
Hans Petter Osvold <osvold89@gmail.com>
2015-04-16 11:47:06 +0200

    Boat test

--- src/model/insurance/vehicle/Vehicle.java
+++ src/model/insurance/vehicle/Vehicle.java
@@ -12,19 +12,18 @@ abstract class Vehicle extends Insurance
 {
     private Person owner;
     private String regNr;
-    private String model;
-    private String regNrRule;
+    //private String model;
+    //private String regNrRule;
 
-    public Vehicle(Person customer, double premium, double amount, Calendar date, String desc, Person owner, String regNr, String model, String regNrRule)
+    public Vehicle(Person customer, double premium, double amount, Calendar date, String desc, Person owner, String regNr)
     {
         super(customer, premium, amount, date, desc);
         this.owner = owner;
         this.regNr = regNr;
-        this.model = model;
-        this.regNrRule = regNrRule;
+
     }
 
     public String toString(){
-        return super.toString() + "\n\n" + "Owner:\t" + owner + "\nRegNr:\t" + regNr + "\nModel:\t" + model + "\nRegNr:\t" +
+        return super.toString() + "\n\n" + "Owner:\t" + owner + "\nRegNr:\t" + regNr;
     }
 }
