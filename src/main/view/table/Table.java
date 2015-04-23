1a0b35facfd51f4d7182bcbd10e10f5138eec254
Alexander Skjolden <alex@plastboks.net>
2015-04-23 11:02:04 +0200

    generic table for displaying data ... - code
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ src/main/view/table/Table.java
@@ -0,0 +1,35 @@
+package main.view.table;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+
+import java.util.List;
+
+/**
+ * Created by alex on 4/23/15.
+ */
+public class Table<T>
+{
+    private List<T> data;
+    private TableView<T> table;
+    private ObservableList<T> observableList;
+
+    public Table(List<T> data)
+    {
+        this.data = data;
+
+        table = new TableView<>();
+        observableList = FXCollections.observableArrayList();
+
+        insertData();
+    }
+
+    private void insertData()
+    {
+        data.stream().forEach(observableList::add);
+        table.setItems(observableList);
+    }
+
+
+}
