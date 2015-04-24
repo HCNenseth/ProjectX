b584678e3ae1e272cd26ff9321863c5a21cf5e6a
Alexander Skjolden <alex@plastboks.net>
2015-04-24 20:39:30 +0200

    Awesome Lambda communication between controller and tableView - code
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/main/controller/Search.java
+++ src/main/controller/Search.java
@@ -5,14 +5,21 @@ import main.localization.Loc;
 import main.model.Storage;
 import main.model.claim.Claim;
 import main.model.insurance.Insurance;
+import main.model.insurance.property.House;
+import main.model.insurance.travel.Travel;
+import main.model.insurance.vehicle.Boat;
+import main.model.insurance.vehicle.Car;
 import main.model.person.Person;
 import main.view.Resources;
 import main.view.concrete.SearchResult;
+import main.view.form.Form;
+import main.view.form.adapter.*;
 import main.view.table.ClaimTable;
 import main.view.table.InsuranceTable;
 import main.view.table.PersonTable;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Created by alex on 4/23/15.
@@ -33,21 +40,95 @@ class Search
         List<Insurance> insurances = (List<Insurance>)Storage.getInstance().get(App.INSURANCES);
         List<Claim> claims = (List<Claim>)Storage.getInstance().get(App.CLAIMS);
 
+        /* person table and actions */
         PersonTable personTable = new PersonTable();
-        InsuranceTable insuranceTable = new InsuranceTable();
-        ClaimTable claimTable = new ClaimTable();
+        personTable.setOnEditAction(this::editPerson);
+        personTable.setOnViewAction(this::viewPerson);
+        personTable.setOnDoubleClickAction(this::viewPerson);
 
+        /* insurance table and actions */
+        InsuranceTable insuranceTable = new InsuranceTable();
+        insuranceTable.setOnEditAction(this::editInsurance);
+        insuranceTable.setOnViewAction(this::viewInsurance);
+        insuranceTable.setOnDoubleClickAction(this::viewInsurance);
+
+        /* claim table and actions */
+        ClaimTable claimTable = new ClaimTable();
+        claimTable.setOnEditAction(this::editClaim);
+        claimTable.setOnViewAction(this::viewClaim);
+        claimTable.setOnDoubleClickAction(this::viewClaim);
+
+        /* generate data for the tables */
         persons.stream().filter(i -> i.query(keyword)).forEach(personTable::insertData);
         insurances.stream().filter(i -> i.query(keyword)).forEach(insuranceTable::insertData);
         claims.stream().filter(i -> i.query(keyword)).forEach(claimTable::insertData);
 
+        /* put the tables into a search result view */
         SearchResult searchResult = new SearchResult();
-
         searchResult.addTable(personTable.getTable(), Loc.get("persons"));
         searchResult.addTable(insuranceTable.getTable(), Loc.get("insurances"));
         searchResult.addTable(claimTable.getTable(), Loc.get("insurances"));
 
+        /* insert search result view into tab */
         Resources.inst.getOtp().injectObservableTab(Loc.get("search_results"),
                 searchResult.getNode(), true);
+    }
+
+    private void viewPerson(Person p)
+    {
+        System.out.println(p.toString());
+    }
+
+    private void editPerson(Person p)
+    {
+        Form f = new Form();
+        f.injectAdapter(new PersonAdapter(p));
+        Resources.inst.getOtp().injectObservableTab(p.getName(),
+                f.getForm(), true);
+    }
+
+    private void viewInsurance(Insurance i)
+    {
+        System.out.println(i.toString());
+    }
+
+    private void editInsurance(Insurance i)
+    {
+        Form f = new Form();
+        switch (i.identify()) {
+            case CAR:
+                f.injectAdapter(new CarAdapter(i.getCustomer(), (Car)i));
+                break;
+            case BOAT:
+                f.injectAdapter(new BoatAdapter(i.getCustomer(), (Boat)i));
+                break;
+            case HOUSE:
+                f.injectAdapter(new HouseAdapter(i.getCustomer(), (House)i));
+                break;
+            case TRAVEL:
+                f.injectAdapter(new TravelAdapter(i.getCustomer(), (Travel)i));
+                break;
+            default: return;
+        }
+
+        Resources.inst.getOtp().injectObservableTab(i.getCustomer().getName(),
+                f.getForm(), true);
+    }
+
+    private void viewClaim(Claim c)
+    {
+        System.out.println(c.toString());
+    }
+
+    private void editClaim(Claim c)
+    {
+        // TODO ClaimAdapter not implemented yet.
+
+        /*
+        Form f = new Form();
+        f.injectAdapter(new ClaimAdapter(c.getCustomer(), c.getInsurance(), c));
+        Resources.inst.getOtp().injectObservableTab(p.getName(),
+                f.getForm(), true);
+        */
     }
 }
--- src/main/view/table/Table.java
+++ src/main/view/table/Table.java
@@ -2,8 +2,14 @@ package main.view.table;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import main.localization.Loc;
+
+import java.util.function.Consumer;
+
 
 /**
  * Created by alex on 4/23/15.
@@ -11,10 +17,44 @@ import javafx.scene.control.TableView;
 public abstract class Table<T> extends TableView<T>
 {
     private ObservableList<T> observableList;
+    private ContextMenu contextMenu;
+    private MenuItem edit;
+    private MenuItem view;
 
     public Table()
     {
+        contextMenu = new ContextMenu();
+        edit = new MenuItem(Loc.get("edit"));
+        view = new MenuItem(Loc.get("view"));
+
+        contextMenu.getItems().add(edit);
+        contextMenu.getItems().add(view);
+
         observableList = FXCollections.observableArrayList();
+
+        setContextMenu(contextMenu);
+    }
+
+    private T getSelected()
+    {
+        return getSelectionModel().getSelectedItem();
+    }
+
+    public void setOnEditAction(Consumer<T> c)
+    {
+        edit.setOnAction(e -> c.accept(getSelected()));
+    }
+
+    public void setOnViewAction(Consumer<T> c)
+    {
+        view.setOnAction(e -> c.accept(getSelected()));
+    }
+
+    public void setOnDoubleClickAction(Consumer<T> c)
+    {
+        onMouseClickedProperty().set(e -> {
+            if (e.getClickCount() == 2) c.accept(getSelected());
+        });
     }
 
     public void insertData(T t)
