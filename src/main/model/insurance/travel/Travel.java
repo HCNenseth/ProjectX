22fcced9b57484438d7b90126374be0979616ef2
HansChristian <nenseth@gmail.com>
2015-04-28 11:24:14 +0200

    Work on TravelAdapter - Code

--- src/main/model/insurance/travel/Travel.java
+++ src/main/model/insurance/travel/Travel.java
@@ -1,10 +1,13 @@
 package main.model.insurance.travel;
 
+import main.localization.Loc;
+import main.model.insurance.Type;
 import main.model.person.Person;
 import main.model.insurance.ConcreteType;
 import main.model.insurance.Insurance;
 import main.model.insurance.InsuranceBuilder;
 
+import javax.management.ValueExp;
 import java.io.Serializable;
 
 /**
@@ -13,13 +16,42 @@ import java.io.Serializable;
 public class Travel extends Insurance implements Serializable
 {
 
-    
+    private final Continent continent;
+
+    public enum Continent
+    {
+        A(Loc.get("continent_a")),
+        B(Loc.get("continent_b")),
+        C(Loc.get("continent_c")),
+        D(Loc.get("continent_d")),
+        E(Loc.get("continent_e")),
+        F(Loc.get("continent_f"));
+
+        String value;
+
+        Continent(String value) { this.value = value;}
+
+        public String getValue() { return value; }
+
+        @Override
+        public String toString() { return value; }
+    }
 
     public static class Builder extends InsuranceBuilder<Builder, Travel>
     {
+
+        private Continent continent = Continent.A;
+
         public Builder(Person customer)
         {
             super.customer(customer);
+        }
+
+
+        public Builder continent(Continent val)
+        {
+            continent = val;
+            return this;
         }
 
         public Travel build()
@@ -28,9 +60,13 @@ public class Travel extends Insurance implements Serializable
         }
     }
 
+
+
+
     private Travel(Builder builder)
     {
         super(builder);
+        continent = builder.continent;
     }
 
     public ConcreteType identify()
