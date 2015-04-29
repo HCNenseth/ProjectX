0e0d05a0bdb32d4acdaf42dcbfa3a83ccce07589
HansChristian <nenseth@gmail.com>
2015-04-29 13:34:34 +0200

    Bugfix in TravelAdapter

--- src/main/config/Config.java
+++ src/main/config/Config.java
@@ -8,7 +8,7 @@ public class Config {
     public static final int HEIGHT = 600;
     public static final int WIDTH = 800;
 
-    public static final String APP_NAME = "Appname be here";
+    public static final String APP_NAME = "Wakizashi";
 
     public static final int STANDARD_YEAR = 1970;
     public static final int STANDARD_MONTH = 1;
