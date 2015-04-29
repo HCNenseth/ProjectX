8b966d2d5ae57e6be161dd0d84231c966daf50a3
Hans Petter Osvold <osvold89@gmail.com>
2015-04-29 10:27:37 +0200

    fuck config? does it work hahaehae

---
+++ src/main/Config.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by HansPetter on 29.04.2015.
+ */
+public class Config {
+
+    public final String APP_NAME = "";
+
+    public final int STANDARD_YEAR = 1970;
+    public final int STANDARD_MONTH = 1;
+    public final int STANDARD_DAY = 1;
+
+}
