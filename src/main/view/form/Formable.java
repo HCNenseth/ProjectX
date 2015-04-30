1ad45203c355b9ff2fcfedd6671b3c4c70f1fade
Alexander Skjolden <alex@plastboks.net>
2015-04-30 12:35:44 +0200

    Better generics definition of Formable - code
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/main/view/form/Formable.java
+++ src/main/view/form/Formable.java
@@ -3,6 +3,7 @@ package main.view.form;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBase;
+import main.model.Model;
 import main.view.form.node.FormNode;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.function.Consumer;
 /**
  * Created by alex on 4/21/15.
  */
-public interface Formable<T>
+public interface Formable<T extends Model>
 {
     // TODO replace this bad event object with a bit more abstract.
     ButtonBase callBackEvent = new ButtonBase()
