06733305647f131885c4642c5c46143ebb2b4c6c
Hans Petter Osvold <osvold89@gmail.com>
2015-05-12 21:22:23 +0200

    dritt

--- src/main/view/concrete/statistics/InsuranceStatisticsView.java
+++ src/main/view/concrete/statistics/InsuranceStatisticsView.java
@@ -2,22 +2,25 @@ package main.view.concrete.statistics;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Side;
 import javafx.scene.chart.*;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import main.localization.Loc;
 import main.model.insurance.Insurance;
 import main.model.insurance.InsuranceType;
 import main.view.StandardGridPane;
 
+import java.awt.event.MouseEvent;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- * Created by alex on 5/7/15.
- */
 public class InsuranceStatisticsView extends StandardGridPane
 {
     private List<Insurance> insurances;
-    private HashMap<String, Integer> data;
+    private HashMap<String, Integer> data, histData;
     private PieChart chart;
 
     public InsuranceStatisticsView(List<Insurance> insurances)
@@ -45,6 +48,62 @@ public class InsuranceStatisticsView extends StandardGridPane
         }
     }
 
+    public boolean isQuarterlyEqual(LocalDate date)
+    {
+        LocalDate b = LocalDate.now();
+        if(date.getYear() == b.getYear())
+        {
+            if( quarter ( date.getMonthValue() ) == quarter(b.getMonthValue() ) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int quarter(int month)
+    {
+        if(month <= 3)
+        {
+            return 1;
+        }
+        else if(month <= 6)
+        {
+            return 2;
+        }
+        else if(month <= 9)
+        {
+            return 3;
+        }
+        else
+        {
+            return 4;
+        }
+    }
+
+    public void generateHistoricData()
+    {
+        histData = new HashMap<>();
+        LocalDate startDate = LocalDate.of(2010, 1, 1);
+        LocalDate endDate = LocalDate.now();
+
+        for(LocalDate date = startDate; date.isBefore(endDate); date = date.plusMonths(3) )
+        {
+
+            for(InsuranceType type : InsuranceType.values() )
+            {
+                histData.put(type.getValue(), insurances.stream()
+                        .filter(i -> i.identify().equals(type)).filter()
+            }
+        }
+
+        for(InsuranceType type : InsuranceType.values()) {
+            histData.put(type.getValue(), (int) insurances.stream()
+                    .filter(i -> i.identify().equals(type)).count());
+        }
+
+    }
+
     public void drawPieChart()
     {
 
@@ -54,6 +113,14 @@ public class InsuranceStatisticsView extends StandardGridPane
             pieChartData.add(new PieChart.Data(e.getKey(), e.getValue())));
 
         chart = new PieChart(pieChartData);
+        chart.setTitle(Loc.c("insurance_distribution"));
+        chart.setLabelsVisible(false);
+        chart.setLegendSide(Side.TOP);
+
+        final Label caption = new Label("");
+        caption.setTextFill(Color.DARKORANGE);
+        caption.setStyle("-fx-font: 24 arial");
+
         add(chart, 0, 0);
     }
 
@@ -64,10 +131,9 @@ public class InsuranceStatisticsView extends StandardGridPane
         final BarChart<String, Number> bc = new BarChart<>(xAxis, yAxis);
         bc.setTitle(Loc.c("summary"));
         xAxis.setLabel(Loc.c("insurance"));
-        yAxis.setLabel(Loc.c("value"));
+        yAxis.setLabel(Loc.c("number"));
 
         XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Total");
 
         data.entrySet().stream().forEach(e ->
                 series1.getData().add(new XYChart.Data<>(e.getKey(), e.getValue())));
@@ -75,6 +141,22 @@ public class InsuranceStatisticsView extends StandardGridPane
         bc.getData().add(series1);
 
         add(bc, 0, 2);
+    }
+
+    public void drawLineChart()
+    {
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel(Loc.c("year"));
+
+        final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+
+        lineChart.setTitle(Loc.c("Total cost per insurance type"));
+
+        XYChart.Series series = new XYChart.Series();
+
+
+
     }
 
     @Override
