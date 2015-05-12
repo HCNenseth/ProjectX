9b06d9dd21d0f01fe4c32d4a8cef11519a062dee
HansChristian <nenseth@gmail.com>
2015-05-12 13:21:51 +0200

    Scaling of P_DialogScene

--- src/main/config/Config.java
+++ src/main/config/Config.java
@@ -19,7 +19,7 @@ public class Config
 
     public static final double HEIGHT = Screen.getPrimary().getVisualBounds().getHeight() * 0.9;
     public static final double WIDTH = Screen.getPrimary().getVisualBounds().getWidth() * 0.9;
-    public static final int PD_HEIGHT = 300, PD_WIDTH = 400;
+    public static final int PD_HEIGHT = 400, PD_WIDTH = 500;
 
     public static final int STANDARD_YEAR = 1970;
     public static final int STANDARD_MONTH = 1;
